dfca1dff6b188777ad278296181ecdde2744ea70
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-23 19:45:53 +0200

    initial project structure

--- src/main/java/com/Trendi/demo/entity/Article.java
+++ src/main/java/com/Trendi/demo/entity/Article.java
@@ -1,4 +1,35 @@
 package com.Trendi.demo.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "articles")
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String imagePath;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+//    @PrePersist ...
+//    @PreUpdate
+
 }
--- src/main/java/com/Trendi/demo/entity/User.java
+++ src/main/java/com/Trendi/demo/entity/User.java
@@ -1,4 +1,43 @@
 package com.Trendi.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
+
+@Entity
+@Table(name="users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+
+    @OneToMany(mappedBy = "author", cascade = ALL, fetch = LAZY)
+    private List<Article> articles;
+
+    @PrePersist void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
 }
