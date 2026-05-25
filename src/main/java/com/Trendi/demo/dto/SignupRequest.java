067dbab76d6b9e0fcad10c5d593fdf0e84e58965
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-25 21:48:03 +0200

    initial project structure

--- src/main/java/com/Trendi/demo/dto/SignupRequest.java
+++ src/main/java/com/Trendi/demo/dto/SignupRequest.java
@@ -1,7 +1,10 @@
 package com.Trendi.demo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class SignupRequest {
 
+    @NotBlank
     private String name;
 
     private String email;
