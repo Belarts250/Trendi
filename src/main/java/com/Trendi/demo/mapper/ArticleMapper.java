c72476c83f8628c30d4722d0e696254ea660d684
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-26 19:56:54 +0200

    initial project structure

--- src/main/java/com/Trendi/demo/mapper/ArticleMapper.java
+++ src/main/java/com/Trendi/demo/mapper/ArticleMapper.java
@@ -1,8 +1,11 @@
 package com.Trendi.demo.mapper;
 
 import com.Trendi.demo.dto.ArticleResponse;
+import com.Trendi.demo.entity.Article;
 
 public class ArticleMapper {
 
-    public static ArticleResponse toResponse
+    public static ArticleResponse toResponse(Article a){
+
+    }
 }
