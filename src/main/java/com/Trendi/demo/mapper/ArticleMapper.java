a1db9dda7457f8b48419b2ccc315a7d1cfeb2245
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-27 19:54:35 +0200

    implementing mapper

--- src/main/java/com/Trendi/demo/mapper/ArticleMapper.java
+++ src/main/java/com/Trendi/demo/mapper/ArticleMapper.java
@@ -1,11 +1,15 @@
 package com.Trendi.demo.mapper;
 
 
+import com.Trendi.demo.dto.ArticleResponse;
+import com.Trendi.demo.entity.Article;
 import lombok.Builder;
 
 @Builder
 public class ArticleMapper {
+ public static ArticleResponse toResponse(Article a){
 
+ }
 
 
 }
