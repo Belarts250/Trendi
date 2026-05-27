1d075f2dae537aecece0e9e89060712f942d750f
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-27 20:04:22 +0200

    implementing mapper

--- src/main/java/com/Trendi/demo/dto/ArticleResponse.java
+++ src/main/java/com/Trendi/demo/dto/ArticleResponse.java
@@ -3,9 +3,11 @@ package com.Trendi.demo.dto;
 import lombok.Builder;
 import lombok.Data;
 
+
 @Builder
 @Data
 public class ArticleResponse {
     private Long id;
     private String title;
+    private String content;
 }
