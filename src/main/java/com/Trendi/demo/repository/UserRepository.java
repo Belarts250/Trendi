f7c31d81144abe36fc4e9d92927c8d227aac2746
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-27 20:16:45 +0200

    implementing repositories

--- src/main/java/com/Trendi/demo/repository/UserRepository.java
+++ src/main/java/com/Trendi/demo/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.Trendi.demo.repository;
 
-public interface UserRepository {
+import com.Trendi.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
