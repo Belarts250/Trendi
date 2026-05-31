fa2aa15fc7af79486fc40597e2e38cdc153a3465
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-31 20:23:20 +0200

    login impplementation

--- src/main/java/com/Trendi/demo/dto/ArticleResponse.java
+++ src/main/java/com/Trendi/demo/dto/ArticleResponse.java
@@ -1,17 +1,24 @@
 package com.Trendi.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ArticleResponse {
     private Long id;
     private String title;
     private String content;
+    private String imagePath;
     private String authorName;
+    private Long authorId;
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/Trendi/demo/dto/AuthResponse.java
+++ src/main/java/com/Trendi/demo/dto/AuthResponse.java
@@ -1,4 +1,10 @@
 package com.Trendi.demo.dto;
 
 public class AuthResponse {
+    private String token;
+    private String tokenType = "Bearer";
+    private Long userId;
+    private String name;
+    private String email;
+
 }
