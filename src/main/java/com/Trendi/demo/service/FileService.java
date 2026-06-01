39b8b348fc51287d53d61f4ac46aecb078af6636
Igihozo Belise <igihozobelise6@gmail.com>
2026-06-01 21:49:12 +0200

    dealing with files

--- src/main/java/com/Trendi/demo/service/FileService.java
+++ src/main/java/com/Trendi/demo/service/FileService.java
@@ -2,7 +2,10 @@ package com.Trendi.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
+
 @Service
 public class FileService {
+    private final Path uploadDir;
 
 }
