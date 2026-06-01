ce111825bf44468be63f01daddf92ec809db9ad5
Igihozo Belise <igihozobelise6@gmail.com>
2026-06-01 14:11:10 +0200

    authentication

--- src/main/java/com/Trendi/demo/controller/AuthController.java
+++ src/main/java/com/Trendi/demo/controller/AuthController.java
@@ -1,4 +1,37 @@
 package com.Trendi.demo.controller;
 
+
+import com.Trendi.demo.dto.AuthResponse;
+import com.Trendi.demo.dto.LoginRequest;
+import com.Trendi.demo.dto.SignupRequest;
+import com.Trendi.demo.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(@Valid @RequestBody SignupRequest request){
+        AuthResponse response = authService.signup(request);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(LoginRequest request){
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
