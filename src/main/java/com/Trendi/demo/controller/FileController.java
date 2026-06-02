c6316d3b185fb1745c6f471fe04c17588ac839f5
Igihozo Belise <igihozobelise6@gmail.com>
2026-06-02 09:16:33 +0200

    controllers implementations

--- src/main/java/com/Trendi/demo/controller/FileController.java
+++ src/main/java/com/Trendi/demo/controller/FileController.java
@@ -1,4 +1,12 @@
 package com.Trendi.demo.controller;
 
+import com.Trendi.demo.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+
 }
