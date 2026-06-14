9eb3a7766858c292f179c1845c4c765f7c3249c2
Igihozo Belise <igihozobelise6@gmail.com>
2026-06-15 00:04:06 +0200

    new

--- src/test/java/com/Trendi/demo/service/ArticleServiceTest.java
+++ src/test/java/com/Trendi/demo/service/ArticleServiceTest.java
@@ -50,7 +50,7 @@ public class ArticleServiceTest {
         mockArticle.setContent("Test Content");
         mockArticle.setAuthor(mockUser);
     }
-//testing files
+//testing
     @Test
     void testCreateArticle() {
         ArticleRequest request = new ArticleRequest();
