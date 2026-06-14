c35cb81284ff0b67a15bf8be89c49c9dec57e7f5
Igihozo Belise <igihozobelise6@gmail.com>
2026-06-14 09:17:54 +0200

    new

--- src/test/java/com/Trendi/demo/service/ArticleServiceTest.java
+++ src/test/java/com/Trendi/demo/service/ArticleServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
+//testing files
 @ExtendWith(MockitoExtension.class)
 public class ArticleServiceTest {
 
